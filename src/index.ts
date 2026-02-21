4caeb3878d35b8b9d67c8f9bdb25f738e14cefdb
Evrige <evrige.game@gmail.com>
2026-02-21 10:02:19 +0200

    v1.4.1 FIX: command create

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,60 @@
-import {Client, GatewayIntentBits, Partials, REST, Routes} from "discord.js";
+import { Client, GatewayIntentBits, Partials, REST, Routes } from "discord.js";
 import * as dotenv from "dotenv";
-import {hiveCommand} from "./commands/detectives/application";
-import {handleInteractions} from "./handlers/interactionHandler";
 import 'dotenv/config';
-import {familyCommand} from "./commands/ravens-family/application";
+
+// Импорт команд
+import { hiveCommand } from "./commands/detectives/application";
+import { familyCommand } from "./commands/ravens-family/application";
+
 dotenv.config();
 
+// Создаем клиента
 const client = new Client({
 	intents: [GatewayIntentBits.Guilds],
 	partials: [Partials.Channel]
 });
+
+// Команды
 const commands = [familyCommand.data.toJSON()];
 const hiveCommands = [hiveCommand.data.toJSON()];
+
+// Настройка серверов и команд для каждого
+const serversCommands = [
+	{
+		guildId: "784348300810780683", // Тестовый сервер
+		commands: [...commands, ...hiveCommands] // объединяем все команды
+	},
+	// Можно добавить другие сервера:
+	// {
+	//     guildId: "ID_другого_сервера",
+	//     commands: [...другие_команды]
+	// }
+];
 
 client.once("ready", async () => {
 	console.log(`Бот запущен как ${client.user?.tag}`);
 
 	const rest = new REST({ version: "10" }).setToken(process.env.TOKEN!);
 
-	// Регистрация команд глобально
-	await rest.put(
-		Routes.applicationGuildCommands(client.user!.id, "784348300810780683"),
-		{ body: commands }
-	);
-	await rest.put(
-		Routes.applicationGuildCommands(client.user!.id, "784348300810780683"),
-		{ body: hiveCommands }
-	);
+	// Регистрируем команды на всех серверах
+	for (const { guildId, commands } of serversCommands) {
+		try {
+			await rest.put(
+				Routes.applicationGuildCommands(client.user!.id, guildId),
+				{ body: commands }
+			);
+			console.log(`Команды успешно зарегистрированы на сервере ${guildId}`);
+		} catch (error) {
+			console.error(`Ошибка при регистрации команд на сервере ${guildId}:`, error);
+		}
+	}
 });
 
+// Обработка взаимодействий
+import { handleInteractions } from "./handlers/interactionHandler";
 client.on("interactionCreate", async (interaction) => {
 	await handleInteractions(interaction);
 });
 
+// Логинимся
 client.login(process.env.TOKEN);
